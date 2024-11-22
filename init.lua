0532cbaf93b3a832a28cea1592a70f11319b1110
Kiril Belazelkovski <kiril.belazelkovski@ingka.ikea.com>
2024-11-22 20:48:36 +0100

    added vim-fugitive

--- init.lua
+++ init.lua
@@ -841,6 +841,7 @@ require('lazy').setup({
     config = function()
       require('catppuccin').setup {
         flavour = 'mocha', -- latte, frappe, macchiato, mocha
+        transparent_background = true,
       }
     end,
     init = function()
@@ -949,6 +950,7 @@ require('lazy').setup({
       { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
     },
   },
+  { 'tpope/vim-fugitive', lazy = false },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
