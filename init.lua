a8384743ae82ba6fad667c72a436c76c44c4893c
Kiril Belazelkovski <kiril.belazelkovski@ingka.ikea.com>
2025-02-28 20:15:26 +0100

    upgrade

--- init.lua
+++ init.lua
@@ -156,6 +156,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 15
 
+vim.opt.spelllang = 'en_us'
+vim.opt.spell = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
