df11dc01b3ddb92c34c2bfd345090c110bcaf0c3
Kiril Belazelkovski <kiril.belazelkovski@ingka.ikea.com>
2025-11-29 10:05:20 +0100

    - enable gopls - add dadbod

--- init.lua
+++ init.lua
@@ -684,7 +684,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -991,7 +991,9 @@ require('lazy').setup({
   require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
-  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns',
+  require 'kickstart.plugins.dadbod',
+  require 'kickstart.plugins.neo-tree',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
---
+++ lua/kickstart/plugins/dadbod.lua
@@ -0,0 +1,61 @@
+local sql_ft = { 'sql', 'mysql', 'plsql' }
+
+return {
+  {
+    'tpope/vim-dadbod',
+    cmd = 'DB',
+  },
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod', lazy = true },
+      {
+        'kristijanhusak/vim-dadbod-completion',
+        ft = sql_ft,
+        lazy = true,
+      },
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    keys = {
+      { '<leader>D', '<cmd>DBUIToggle<CR>', desc = 'Toggle DBUI' },
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = sql_ft,
+        callback = function()
+          local cmp = require 'cmp'
+
+          -- global sources
+          ---@param source cmp.SourceConfig
+          local sources = vim.tbl_map(function(source)
+            return { name = source.name }
+          end, cmp.get_config().sources)
+
+          -- add vim-dadbod-completion source
+          table.insert(sources, { name = 'vim-dadbod-completion' })
+
+          -- update sources for the current buffer
+          cmp.setup.buffer { sources = sources }
+        end,
+      })
+
+      local data_path = vim.fn.stdpath 'data'
+
+      vim.g.db_ui_auto_execute_table_helpers = 1
+      vim.g.db_ui_save_location = data_path .. '/dadbod_ui'
+      vim.g.db_ui_show_database_icon = true
+      vim.g.db_ui_tmp_query_location = data_path .. '/dadbod_ui/tmp'
+      vim.g.db_ui_use_nerd_fonts = true
+      vim.g.db_ui_use_nvim_notify = true
+      vim.g.db_ui_execute_on_save = false
+    end,
+  },
+}
