edf2830d2955739f72d0abb10da3c8a9e014cdac
Kiril Belazelkovski <kiril.belazelkovski@ingka.ikea.com>
2025-12-07 20:09:16 +0100

    updates

--- init.lua
+++ init.lua
@@ -701,13 +701,13 @@ require('lazy').setup({
           -- filetypes = { ... },
           -- capabilities = {},
           settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
-            },
+            -- Lua = {
+            --   completion = {
+            --     callSnippet = 'Replace',
+            --   },
+            --   -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+            --   -- diagnostics = { disable = { 'missing-fields' } },
+            -- },
           },
         },
       }
